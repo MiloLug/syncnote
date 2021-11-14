03eb15e74c7b22a611d3edd892ba26efb62d8d25
Lia Va <mistickusya.2012@gmail.com>
2021-11-14 23:11:53 +0200

    a

---
+++ src/store/loading_screen.js
@@ -0,0 +1,27 @@
+export default {
+    namespaced: true,
+    state: () => ({
+        demandersNumber: 0,  // shows how many times the screen was demanded to show up
+        show: false
+    }),
+    mutations: {
+        show(state) {
+            state.demandersNumber++;
+            state.show = true;
+        },
+
+        hide(state, forced=false) {
+            if(state.demandersNumber === 0)
+                return;
+
+            if(forced)
+                state.show = state.demandersNumber = 0;
+            else
+                state.show = !! --state.demandersNumber;
+        }
+    },
+    actions: {
+    },
+    modules: {
+    }
+};
